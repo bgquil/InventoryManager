8d3acb8cdf1fb6308e69e7c56bb30b56ad49d199
Benjamin Quilliams <bgquilliams@gmail.com>
2018-08-03 14:36:27 -0400

    renamed

--- REST/api/db/manufacturersDB.js
+++ REST/api/db/manufacturersDB.js
@@ -1,11 +1,10 @@
 const db = require('./dbConfig');
 
 
-exports.getManufacturer = (manufacturerID) => {
-
+exports.getManufacturer = (id) => {
   return new Promise((resolve, reject) => {
-    const stmt = 'SELECT * FROM manufacturers WHERE manufacturerID = ?;';
-    db.query(stmt, manufacturerID, (err, result) => {
+    const stmt = 'SELECT * FROM manufacturers WHERE manufacturerID = ?';
+    db.query(stmt, id, (err, result) => {
       if (err) {
         return reject(err);
       }
@@ -15,7 +14,18 @@ exports.getManufacturer = (manufacturerID) => {
 };
 
 exports.getAllManufacturers = () => {
+  return new Promise((resolve, reject) => {
+    const stmt = 'SELECT * FROM manufacturers;';
+    db.query(stmt, (err, result) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(result);
+    });
+  });
+};
 
+exports.addManufacturer = (name) => {
   return new Promise((resolve, reject) => {
     const stmt = 'SELECT * FROM manufacturers;';
     db.query(stmt, (err, result) => {
