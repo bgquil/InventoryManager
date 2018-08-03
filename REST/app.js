eef86992e649a0bd8189d4ca6c12a40f67f21bb0
Benjamin Quilliams <bgquilliams@gmail.com>
2018-08-03 19:47:06 -0400

    add body-parser

--- REST/app.js
+++ REST/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 
 const manufacturersRoute = require('./api/routes/manufacturers');
 const itemsRoute = require('./api/routes/items.js');
