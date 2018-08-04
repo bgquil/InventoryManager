0cd1152663ae9d4fcd457202913f1f9a9b4ca866
Benjamin Quilliams <bgquilliams@gmail.com>
2018-08-04 00:16:22 -0400

    Begin orders implementation

--- REST/api/db/ordersDB.js
+++ REST/api/db/ordersDB.js
@@ -1,2 +1,25 @@
 const db = require('./dbConfig');
 
+exports.getOrders = () => {
+  return new Promise((resolve, reject) => {
+    const orderStmt = 'SELECT * FROM orders LIMIT 10;';
+    db.query(orderStmt, (err, result) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(result);
+    });
+  });
+};
+
+exports.getOrder = (orderID) => {
+  return new Promise((resolve, reject) => {
+    const orderStmt = 'SELECT * FROM orders WHERE orderID = ?;';
+    db.query(orderStmt, orderID,  (err, result) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(result);
+    });
+  });
+};
--- REST/api/routes/orders.js
+++ REST/api/routes/orders.js
@@ -4,9 +4,25 @@ const router = express.Router();
 const ordersDB = require('../db/ordersDB');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: "order get placeholder"
-    })
+    ordersDB.getOrders().then((data) => {
+        res.status(200).json(data);
+    }).catch(err => setImmediate(() => { throw err;}))
 });
 
+router.get('/:orderID', (req, res, next) => {
+    ordersDB.getOrder(req.params.orderID).then((data) => {
+        res.status(200).json(data);
+    }).catch(err => setImmediate(() => { throw err;}))
+
+});
+
+router.post('/', (req, res, next) => {
+    const newOrder = {
+        message: 'test'
+    }
+    ordersDB.addOrder(newOrder).then((data) => {
+        res.status(200).json(data);
+    }).catch(err => setImmediate(() => { throw err;}))
+
+});
 module.exports = router;
