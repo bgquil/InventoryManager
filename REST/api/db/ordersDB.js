a3ff8c790f4fdc89533a986b2a5bed897801e417
Benjamin Quilliams <bgquilliams@gmail.com>
2018-08-04 21:48:52 -0400

    Implemented order data and order lists

--- REST/api/db/ordersDB.js
+++ REST/api/db/ordersDB.js
@@ -12,10 +12,30 @@ exports.getOrders = () => {
   });
 };
 
-exports.getOrder = (orderID) => {
+exports.getOrderData = (orderID) => {
   return new Promise((resolve, reject) => {
     const orderStmt = 'SELECT * FROM orders WHERE orderID = ?;';
     db.query(orderStmt, orderID,  (err, result) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(result[0]);
+    });
+  });
+};
+
+// Get items within an order
+exports.getOrderItems = (orderID) => {
+  const itemStmt = 'SELECT items.itemID, items.name, items.model,\
+                        manufacturers.manufacturerID, manufacturers.manufacturerName,\
+                        order_item.quantityOrdered\
+                        FROM order_item\
+                        RIGHT JOIN items ON order_item.itemID = items.itemID\
+                        JOIN manufacturers ON items.manufacturerID = manufacturers.manufacturerID\
+                        WHERE order_item.orderID = ?';
+
+  return new Promise((resolve, reject) => {
+    db.query(itemStmt, orderID, (err, result) => {
       if (err) {
         return reject(err);
       }
@@ -24,6 +44,9 @@ exports.getOrder = (orderID) => {
   });
 };
 
+exports.getOrder = (orderID) => {
+  return Promise.all([exports.getOrderData(orderID), exports.getOrderItems(orderID)]);
+};
 // Create a new order inserting its total quantity and return its ID.
 exports.createOrder = (totalQuantity) => {
   return new Promise( (reject, resolve) => {
@@ -52,20 +75,3 @@ exports.setupOrder = (orderList) => {
   }).catch(err => setImmediate((err) => {throw err}));
 };
 
-
-// Get items within an order
-exports.getOrderItems = (orderID) => {
-  const itemStmt = 'SELECT * FROM order_item\
-                        INNER JOIN orders ON orders.OrderID = order_item.OrderID\
-                        INNER JOIN items ON order_item.itemID = items.itemID\
-                        INNER JOIN manufacturers ON items.manufacturerID = manufacturers.manufacturerID\
-                        WHERE order_item.orderID = ?';
-  return new Promise((resolve, reject) => {
-    db.query(itemStmt, orderID, (err, result) => {
-      if (err) {
-        return reject(err);
-      }
-      return resolve(result);
-    });
-  });
-};
--- REST/api/routes/orders.js
+++ REST/api/routes/orders.js
@@ -3,15 +3,28 @@ const router = express.Router();
 
 const ordersDB = require('../db/ordersDB');
 
+// get a list of orders
 router.get('/', (req, res, next) => {
     ordersDB.getOrders().then((data) => {
         res.status(200).json(data);
     }).catch(err => setImmediate(() => { throw err;}))
 });
 
+// get order metadata
 router.get('/:orderID', (req, res, next) => {
-    ordersDB.getOrder(req.params.orderID).then((data) => {
-        res.status(200).json(data);
+    ordersDB.getOrderData(req.params.orderID).then((orderData) => {
+        res.status(200).json(orderData);
+    }).catch(err => setImmediate(() => { throw err;}))
+
+});
+
+// return order metadata and order items list
+router.get('/:orderID/invoice', (req, res, next) => {
+    ordersDB.getOrder(req.params.orderID).then((order) => {
+        res.status(200).json({
+            orderData: order[0],
+            orderItems: order[1],
+        });
     }).catch(err => setImmediate(() => { throw err;}))
 
 });
