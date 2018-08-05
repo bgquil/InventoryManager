badd716475279fe011104eaf58cde8384cef0354
Benjamin Quilliams <bgquilliams@gmail.com>
2018-08-04 20:47:18 -0400

    Add headers

--- REST/app.js
+++ REST/app.js
@@ -7,7 +7,15 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept Authorization');
+    if (req.method ===  'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 const manufacturersRoute = require('./api/routes/manufacturers');
 const itemsRoute = require('./api/routes/items.js');
