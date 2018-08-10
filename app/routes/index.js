ab5fff7dd5314bc841708e91adc9d6ad4fc94cb1
Benjamin Quilliams <bgquilliams@gmail.com>
2018-08-09 22:45:46 -0400

    Updated rest

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,11 @@
 // Import database connection
 const ordersDB = require('./db/ordersDB');
+const restService = require('../restService/restService');
 
 // Get Index
 exports.index = (req, res) => {
-  ordersDB.getRecentOrders().then((data) => {
-    res.render('index', { title: 'Root Index', orderData: data });
-  }).catch((err) => setImmediate(() => { throw err; }));
+  const openPath = '/orders/open'
+  restService.getRequest(openPath, (err, result) => {
+    res.render('index', { title: 'Root Index', orderData: result });
+  });
 };
