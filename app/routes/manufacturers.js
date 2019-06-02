efc8519a8c971506ef6dff520aaced9adbe271f5
Benjamin Quilliams <bgquilliams@gmail.com>
2019-06-02 14:11:37 -0400

    Reorganize

--- app/routes/manufacturers.js
+++ app/routes/manufacturers.js
@@ -3,23 +3,11 @@ const db = require('../config/db');
 const restService = require('../restService/restService');
 const qs = require('querystring');
 
+/*
 
-const createManufacturer = (formData) => {
-  const input = JSON.parse(JSON.stringify(formData));
-  console.log(input);
-  let manufacturer = {
-    manufacturerID: input.manufacturerID, 
-    manufacturerName: input.manufacturerName
-  };
-  return manufacturer;
-};
+### Render ###
 
-const verifyManufacturer = (manufacturer) => {
-  // todo
-  //const id = parseInt(req.params.id, 10);
-  return true;
-};
-
+*/
 
 // Render the manufacturers main view.
 exports.manufacturersMain = (req, res) => {
@@ -40,28 +28,29 @@ exports.addManufacturer = (req, res) => {
   const manufacturer_form_data = qs.stringify({ 
     manufacturerName: input.manufacturerName,
   });
+  console.log(manufacturer_form_data);
   const apiPath = '/manufacturers/add'; 
   restService.postRequest(apiPath, manufacturer_form_data);
   res.redirect('/manufacturers');
 };
 
-// Remove a manufacturer from the database.
-exports.deleteManufacturer = (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  if (id >= 0 && id != NaN) {
-    const apiPath = '/manufacturers/' + req.params.id + '/delete';
-    console.log(apiPath)
-    restService.deleteRequest(apiPath,  req.params.id, (err, result) => {
-      if (err)
-        throw err;
-      console.log(result);
-      res.redirect('/manufacturers');
+// Single manufacturer overview with items
+exports.viewManufacturer = (req, res) => {
+  const manufacturerID = req.params.id;
+  const manufacturerPath = '/manufacturers/' + manufacturerID;
+  const manufacturerItemsPath = '/manufacturers/' + manufacturerID + '/items';
+
+  restService.getRequest(manufacturerPath, (err, manufacturerData) => {
+    restService.getRequest(manufacturerItemsPath, (err, itemData) => {
+      res.render('manufacturers/view_manufacturer', {
+        title: 'Manufacturer Overview',
+        manufacturerData,
+        itemData
+      });
     });
-  }
-  else {
-      res.redirect('/manufacturers/edit' + id);
-  }
+  });
 };
+
 
 // Render the edit manufacturer view
 exports.renderEdit = (req, res) => {
@@ -74,6 +63,30 @@ exports.renderEdit = (req, res) => {
     });
 
   });
+};
+
+
+
+/*
+
+### Actions ###
+
+*/
+
+const createManufacturer = (formData) => {
+  const input = JSON.parse(JSON.stringify(formData));
+  console.log(input);
+  let manufacturer = {
+    manufacturerID: input.manufacturerID, 
+    manufacturerName: input.manufacturerName
+  };
+  return manufacturer;
+};
+
+const verifyManufacturer = (manufacturer) => {
+  // todo
+  //const id = parseInt(req.params.id, 10);
+  return true;
 };
 
 // Apply edit to manufacturer
@@ -95,22 +108,22 @@ exports.editManufacturer = (req, res) => {
     console.log(result);
     res.redirect('/manufacturers/');
   });
-
 };
 
-// Manufacturer Overview with Items
-exports.viewManufacturer = (req, res) => {
-  const manufacturerID = req.params.id;
-  const manufacturerPath = '/manufacturers/' + manufacturerID;
-  const manufacturerItemsPath = '/manufacturers/' + manufacturerID + '/items';
-
-  restService.getRequest(manufacturerPath, (err, manufacturerData) => {
-    restService.getRequest(manufacturerItemsPath, (err, itemData) => {
-      res.render('manufacturers/view_manufacturer', {
-        title: 'Manufacturer Overview',
-        manufacturerData,
-        itemData
-      });
+// Remove a manufacturer from the database.
+exports.deleteManufacturer = (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (id >= 0 && id != NaN) {
+    const apiPath = '/manufacturers/' + req.params.id + '/delete';
+    console.log(apiPath)
+    restService.deleteRequest(apiPath,  req.params.id, (err, result) => {
+      if (err)
+        throw err;
+      console.log(result);
+      res.redirect('/manufacturers');
     });
-  });
+  }
+  else {
+      res.redirect('/manufacturers/edit' + id);
+  }
 };
